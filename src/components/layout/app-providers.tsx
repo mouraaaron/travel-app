2459f75b3977051525192403d75c2f872b91229d
Aaron Moura <moura.aaron06@gmail.com>
2026-07-10 10:50:24 -0300

    refactor: drop localStorage-backed request providers from AppProviders

--- src/components/layout/app-providers.tsx
+++ src/components/layout/app-providers.tsx
@@ -1,22 +1,17 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { RequestsProvider, TravelRequestsProvider } from "@/lib/requests-store";
 import { TripFlowProvider } from "@/lib/trip-flow-store";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export function AppProviders({ children }: { children: ReactNode }) {
   return (
-    <TravelRequestsProvider>
-      <RequestsProvider>
-        <TripFlowProvider>
-          <TooltipProvider delayDuration={150}>
-            {children}
-            <Toaster position="bottom-center" />
-          </TooltipProvider>
-        </TripFlowProvider>
-      </RequestsProvider>
-    </TravelRequestsProvider>
+    <TripFlowProvider>
+      <TooltipProvider delayDuration={150}>
+        {children}
+        <Toaster position="bottom-center" />
+      </TooltipProvider>
+    </TripFlowProvider>
   );
 }
